c5497e0966fbdbc9b9393d77fdf58aa1116c2d61
Pasindu41555-pasiiii <Pasindulakshitha82@gmail.com>
2025-03-10 22:51:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
+  packname: 'Queen Oshii',
+  author: '',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Oshadi Rajapaksha', // Your name
+  ownerNumber: '94759892523', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
